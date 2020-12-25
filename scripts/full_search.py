38c68c2ff8065ff6d7d84b1d7133ffd164597a84
Jeremy Smith <j.smith.03@cantab.net>
2020-12-25 15:52:41 -0800

    Decomposed data functions and cleaned up full search script

--- scripts/full_search.py
+++ scripts/full_search.py
@@ -9,18 +9,17 @@ occur before and after a chosen target.
 -hr : How many hours to search (for testing)
 """
 import argparse
-import bz2
-import json
 import multiprocessing
 import os
 import pandas as pd
 from unicode_codes import EMOJI_UNICODE
 from timeit import default_timer as timer
 from tqdm import tqdm
-from twitter_search_funcs import find_context, sum_dicts
+from twitter_search import find_context, sum_dicts
+from twitter_search.data import read_zip
 
 # Character to match
-match = EMOJI_UNICODE[':pistol:']
+match = EMOJI_UNICODE[":pistol:"]
 
 # Global counters
 counter_total_tweets = 0
@@ -44,56 +43,42 @@ def worker(filename):
         dict: result_dict
     """
     result_dict = {}
-    result_dict['counter_total_tweets'] = 0
-    result_dict['counter_total_match'] = 0
-    result_dict['counter_total_before'] = 0
-    result_dict['counter_total_after'] = 0
-    result_dict['counterdict_before'] = {}
-    result_dict['counterdict_after'] = {}
-    result_dict['counterdict_lang'] = {}
+    result_dict["counter_total_tweets"] = 0
+    result_dict["counter_total_match"] = 0
+    result_dict["counter_total_before"] = 0
+    result_dict["counter_total_after"] = 0
+    result_dict["counterdict_before"] = {}
+    result_dict["counterdict_after"] = {}
+    result_dict["counterdict_lang"] = {}
 
-    fbz = bz2.BZ2File(filename, 'rb')
-    try:
-        fdec = fbz.read()
-    except IOError:
-        return None
-    finally:
-        fbz.close()
-    fdecutf = fdec.decode('utf-8')
+    for tweet in read_zip(filename):
 
-    for t in fdecutf.split('\n'):
-        try:
-            tweet = json.loads(t)
-        except (ValueError):
-            continue
-        if 'delete' in tweet.keys():
-            continue
-        result_dict['counter_total_tweets'] += 1
-        if match in tweet['text']:
-            result_dict['counter_total_match'] += 1
-            result = find_context(tweet['text'], match)
+        result_dict["counter_total_tweets"] += 1
+        if match in tweet["text"]:
+            result_dict["counter_total_match"] += 1
+            result = find_context(tweet["text"], match)
 
             if result[0] in EMOJI_UNICODE.values():
-                result_dict['counter_total_before'] += 1
+                result_dict["counter_total_before"] += 1
 
-                if result[0] in result_dict['counterdict_before'].keys():
-                    result_dict['counterdict_before'][result[0]] += 1
+                if result[0] in result_dict["counterdict_before"].keys():
+                    result_dict["counterdict_before"][result[0]] += 1
                 else:
-                    result_dict['counterdict_before'][result[0]] = 1
+                    result_dict["counterdict_before"][result[0]] = 1
 
             if result[2] in EMOJI_UNICODE.values():
-                result_dict['counter_total_after'] += 1
+                result_dict["counter_total_after"] += 1
 
-                if result[2] in result_dict['counterdict_after'].keys():
-                    result_dict['counterdict_after'][result[2]] += 1
+                if result[2] in result_dict["counterdict_after"].keys():
+                    result_dict["counterdict_after"][result[2]] += 1
                 else:
-                    result_dict['counterdict_after'][result[2]] = 1
+                    result_dict["counterdict_after"][result[2]] = 1
 
             try:
-                if tweet['lang'] in result_dict['counterdict_lang'].keys():
-                    result_dict['counterdict_lang'][tweet['lang']] += 1
+                if tweet["lang"] in result_dict["counterdict_lang"].keys():
+                    result_dict["counterdict_lang"][tweet["lang"]] += 1
                 else:
-                    result_dict['counterdict_lang'][tweet['lang']] = 1
+                    result_dict["counterdict_lang"][tweet["lang"]] = 1
             except KeyError:
                 continue
 
@@ -103,18 +88,21 @@ def worker(filename):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Search a Twitter archive (from archive.org) to find the "
-                    "characters which occur before and after a chosen target",
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        "characters which occur before and after a chosen target",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
     parser.add_argument(
-        '-p', '--data_path',
+        "-p",
+        "--data_path",
         default="/your/data/path/archive-twitter-2016-08/",
-        help="Path to the Twitter archive")
+        help="Path to the Twitter archive",
+    )
     parser.add_argument(
-        '-d', '--days', type=int, default=31,
-        help="How many days to search (for testing)")
+        "-d", "--days", type=int, default=31, help="How many days to search (for testing)"
+    )
     parser.add_argument(
-        '-hr', '--hours', type=int, default=24,
-        help="How many hours to search (for testing)")
+        "-hr", "--hours", type=int, default=24, help="How many hours to search (for testing)"
+    )
     args = parser.parse_args()
 
     number_of_processes = multiprocessing.cpu_count()
@@ -137,28 +125,20 @@ if __name__ == "__main__":
 
     pool = multiprocessing.Pool(number_of_processes)
     try:
-        for result_dict in tqdm(pool.imap_unordered(worker,
-                                                    all_files,
-                                                    chunksize=10),
-                                total=len(all_files),
-                                unit="files"):
+        for result_dict in tqdm(
+            pool.imap_unordered(worker, all_files, chunksize=10), total=len(all_files), unit="files"
+        ):
 
             if result_dict is None:
                 continue
 
-            counter_total_tweets += result_dict['counter_total_tweets']
-            counter_total_match += result_dict['counter_total_match']
-            counter_total_before += result_dict['counter_total_before']
-            counter_total_after += result_dict['counter_total_after']
-            counterdict_before = sum_dicts(
-                counterdict_before,
-                result_dict['counterdict_before'])
-            counterdict_after = sum_dicts(
-                counterdict_after,
-                result_dict['counterdict_after'])
-            counterdict_lang = sum_dicts(
-                counterdict_lang,
-                result_dict['counterdict_lang'])
+            counter_total_tweets += result_dict["counter_total_tweets"]
+            counter_total_match += result_dict["counter_total_match"]
+            counter_total_before += result_dict["counter_total_before"]
+            counter_total_after += result_dict["counter_total_after"]
+            counterdict_before = sum_dicts(counterdict_before, result_dict["counterdict_before"])
+            counterdict_after = sum_dicts(counterdict_after, result_dict["counterdict_after"])
+            counterdict_lang = sum_dicts(counterdict_lang, result_dict["counterdict_lang"])
 
     except KeyboardInterrupt:
         print("KeyboardInterrupt")
@@ -176,23 +156,18 @@ if __name__ == "__main__":
     print("Total w/ After  : {:d}".format(counter_total_after))
 
     # Convert output to dataframe
-    df_before = pd.DataFrame(
-        list(counterdict_before.items()), columns=['Emoji', 'CountBefore'])
-    df_after = pd.DataFrame(
-        list(counterdict_after.items()), columns=['Emoji', 'CountAfter'])
-    df_lang = pd.DataFrame(
-        list(counterdict_lang.items()), columns=['Lang', 'Count'])
+    df_before = pd.DataFrame(list(counterdict_before.items()), columns=["Emoji", "CountBefore"])
+    df_after = pd.DataFrame(list(counterdict_after.items()), columns=["Emoji", "CountAfter"])
+    df_lang = pd.DataFrame(list(counterdict_lang.items()), columns=["Lang", "Count"])
 
     # Merge before and after dataframes
-    df_all = pd.merge(df_before, df_after, on='Emoji', how='outer')
+    df_all = pd.merge(df_before, df_after, on="Emoji", how="outer")
 
-    df_all.sort_values('CountBefore', ascending=False).head()
-    df_all.sort_values('CountAfter', ascending=False).head()
-    df_lang.sort_values('Count', ascending=False).head()
-    df_all.sort_values('CountBefore',
-                       ascending=False).head(20).plot.bar(y='CountBefore')
-    df_all.sort_values('CountAfter',
-                       ascending=False).head(20).plot.bar(y='CountAfter')
+    df_all.sort_values("CountBefore", ascending=False).head()
+    df_all.sort_values("CountAfter", ascending=False).head()
+    df_lang.sort_values("Count", ascending=False).head()
+    df_all.sort_values("CountBefore", ascending=False).head(20).plot.bar(y="CountBefore")
+    df_all.sort_values("CountAfter", ascending=False).head(20).plot.bar(y="CountAfter")
 
     # Export results as CSV files
     df_all.to_csv("./alldata.csv", encoding="utf-8")
--- scripts/full_search_total_count.py
+++ scripts/full_search_total_count.py
@@ -9,225 +9,221 @@ occur before and after a chosen target. Also counts total number of emoji charac
 -hr : How many hours to search (for testing)
 """
 import argparse
-import bz2
-import json
 import multiprocessing
-import os
 import pandas as pd
-from unicode_codes import EMOJI_UNICODE
 from timeit import default_timer as timer
 from tqdm import tqdm
-from twitter_search_funcs import find_context, find_all, sum_dicts
 
-# Character to match
-match = EMOJI_UNICODE[':pistol:']
+from twitter_search import find_context, find_all, sum_dicts
+from twitter_search.data import read_zip, unpack_files, get_all_files
+from twitter_search.unicode_codes import EMOJI_UNICODE
 
-# Global counters
-counter_total_tweets = 0
-counter_total_tweets_wemoji = 0
-counter_total_match = 0
-counter_total_before = 0
-counter_total_after = 0
-counterdict_before = {}
-counterdict_after = {}
-counterdict_lang = {}
-counterdict_all_emoji = {}
+
+class Results:
+
+    """Search results data class.
+
+    Attributes:
+        counter_total_after (int): Total number of tweets with emoji after the match character
+        counter_total_before (int): Total number of tweets with emoji before the match character
+        counter_total_match (int): Total number of tweets with the match character
+        counter_total_tweets (int): Total number of tweets
+        counter_total_tweets_wemoji (int): Total number of tweets with any emoji
+        counterdict_after (dict): Distribution of emoji after the match character
+        counterdict_before (dict): Distribution of emoji before the match character
+        counterdict_lang (dict): Distribution of tweet languages
+        counterdict_all_emoji (dict): Distribution of all emoji
+    """
+
+    def __init__(self):
+        """Initialize all counters to 0 and counter dicts to be empty."""
+        self.counter_total_tweets = 0
+        self.counter_total_tweets_wemoji = 0
+        self.counter_total_match = 0
+        self.counter_total_before = 0
+        self.counter_total_after = 0
+
+        self.counterdict_before = {}
+        self.counterdict_after = {}
+        self.counterdict_lang = {}
+        self.counterdict_all_emoji = {}
 
 
 def worker(filename):
-    """The worker function, invoked in a process. 'filename' is a
-    zipped file of tweets to process. Results will be returned in
-    'result_dict'
+    """The worker function, invoked in a process.
 
     Args:
         filename (str): Zipped file of tweets to process
 
     Returns:
-        dict: result_dict
+        Results
     """
-    result_dict = {}
-    result_dict['counter_total_tweets'] = 0
-    result_dict['counter_total_tweets_wemoji'] = 0
-    result_dict['counter_total_match'] = 0
-    result_dict['counter_total_before'] = 0
-    result_dict['counter_total_after'] = 0
-    result_dict['counterdict_before'] = {}
-    result_dict['counterdict_after'] = {}
-    result_dict['counterdict_lang'] = {}
-    result_dict['counterdict_all_emoji'] = {}
+    results = Results()
 
-    # Decompress and decode .bz2 file
-    fbz = bz2.BZ2File(filename, 'rb')
-    try:
-        fdec = fbz.read()
-    except IOError:
-        return None
-    finally:
-        fbz.close()
-    fdecutf = fdec.decode('utf-8')
-
-    # Loop through each line in file
-    for t in fdecutf.split('\n'):
-        # Load and check for valid tweet
-        try:
-            tweet = json.loads(t)
-        except (ValueError):
-            continue
-        if 'delete' in tweet.keys():
-            continue
+    for tweet in read_zip(filename):
 
         # Count total number of tweets
-        result_dict['counter_total_tweets'] += 1
+        results.counter_total_tweets += 1
 
         # Count total numbers of emoji in tweet
-        all_emoji, all_count = find_all(tweet['text'])
+        all_emoji, all_count = find_all(tweet["text"])
         if not all_emoji:
             continue
-        result_dict['counter_total_tweets_wemoji'] += 1
+        results.counter_total_tweets_wemoji += 1
         for i, c in enumerate(all_emoji):
-            if c in result_dict['counterdict_all_emoji'].keys():
-                result_dict['counterdict_all_emoji'][c] += all_count[i]
+            if c in results.counterdict_all_emoji.keys():
+                results.counterdict_all_emoji[c] += all_count[i]
             else:
-                result_dict['counterdict_all_emoji'][c] = all_count[i]
+                results.counterdict_all_emoji[c] = all_count[i]
 
         # Count number and context of match emoji
-        if match in all_emoji:
-            result_dict['counter_total_match'] += 1
-            result = find_context(tweet['text'], match)
+        if MATCH in all_emoji:
+            results.counter_total_match += 1
+            result = find_context(tweet["text"], MATCH)
 
             # Before match
             if result[0] in EMOJI_UNICODE.values():
-                result_dict['counter_total_before'] += 1
+                results.counter_total_before += 1
 
-                if result[0] in result_dict['counterdict_before'].keys():
-                    result_dict['counterdict_before'][result[0]] += 1
+                if result[0] in results.counterdict_before.keys():
+                    results.counterdict_before[result[0]] += 1
                 else:
-                    result_dict['counterdict_before'][result[0]] = 1
+                    results.counterdict_before[result[0]] = 1
             # After match
             if result[2] in EMOJI_UNICODE.values():
-                result_dict['counter_total_after'] += 1
+                results.counter_total_after += 1
 
-                if result[2] in result_dict['counterdict_after'].keys():
-                    result_dict['counterdict_after'][result[2]] += 1
+                if result[2] in results.counterdict_after.keys():
+                    results.counterdict_after[result[2]] += 1
                 else:
-                    result_dict['counterdict_after'][result[2]] = 1
+                    results.counterdict_after[result[2]] = 1
 
             try:
-                if tweet['lang'] in result_dict['counterdict_lang'].keys():
-                    result_dict['counterdict_lang'][tweet['lang']] += 1
+                if tweet["lang"] in results.counterdict_lang.keys():
+                    results.counterdict_lang[tweet["lang"]] += 1
                 else:
-                    result_dict['counterdict_lang'][tweet['lang']] = 1
+                    results.counterdict_lang[tweet["lang"]] = 1
             except KeyError:
                 continue
 
-    return result_dict
+    return results
 
 
-if __name__ == "__main__":
+def parse_cli_args():
+    """Parse the require CLI arguments for the run.
+
+    Returns:
+        argparse.Namespace
+    """
     parser = argparse.ArgumentParser(
-        description="Search a Twitter archive (from archive.org) to find the "
-                    "characters which occur before and after a chosen target",
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        description="Search a Twitter archive (from archive.org)",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
     parser.add_argument(
-        '-p', '--data_path',
+        "-p",
+        "--data_path",
         default="/your/data/path/archive-twitter-2016-08/",
-        help="Path to the Twitter archive")
+        help="Path to the Twitter archive",
+    )
     parser.add_argument(
-        '-d', '--days', type=int, default=31,
-        help="How many days to search (for testing)")
+        "-d", "--days", type=int, default=31, help="How many days to search (for testing)"
+    )
     parser.add_argument(
-        '-hr', '--hours', type=int, default=24,
-        help="How many hours to search (for testing)")
-    args = parser.parse_args()
+        "-hr", "--hours", type=int, default=24, help="How many hours to search (for testing)"
+    )
+    return parser.parse_args()
 
+
+def run():
+    """Run the full search.
+
+    Returns:
+        Results
+    """
+    start_t = timer()
+    # Global counters
+    results_global = Results()
     # Set multiprocessing cpu count
     number_of_processes = multiprocessing.cpu_count()
     multiprocessing.freeze_support()  # Prevent an error on Windows
-
-    # Main search loop
-    start_t = timer()
-    all_files = []
-    for day in range(args.days):
-        day_str = "{:02d}".format(day + 1)
-        for hour in range(args.hours):
-            hour_str = "{:02d}".format(hour)
-            file_path = os.path.join(args.data_path, day_str, hour_str)
-            files = os.listdir(file_path)
-
-            for i, f in enumerate(files):
-                files[i] = os.path.join(file_path, f)
-
-            all_files.extend(files)
-
     # Create pool of processes
     pool = multiprocessing.Pool(number_of_processes)
     try:
         # Run worker functions and use tqdm progress bar
-        for result_dict in tqdm(pool.imap_unordered(worker,
-                                                    all_files,
-                                                    chunksize=10),
-                                total=len(all_files),
-                                unit="files"):
-
-            if result_dict is None:
+        processes = pool.imap_unordered(worker, all_files, chunksize=10)
+        for results in tqdm(processes, total=len(all_files), unit="files"):
+            if results is None:
                 continue
             # Update all global counters
-            counter_total_tweets += result_dict['counter_total_tweets']
-            counter_total_tweets_wemoji += result_dict['counter_total_tweets_wemoji']
-            counter_total_match += result_dict['counter_total_match']
-            counter_total_before += result_dict['counter_total_before']
-            counter_total_after += result_dict['counter_total_after']
-            counterdict_before = sum_dicts(
-                counterdict_before,
-                result_dict['counterdict_before'])
-            counterdict_after = sum_dicts(
-                counterdict_after,
-                result_dict['counterdict_after'])
-            counterdict_lang = sum_dicts(
-                counterdict_lang,
-                result_dict['counterdict_lang'])
-            counterdict_all_emoji = sum_dicts(
-                counterdict_all_emoji,
-                result_dict['counterdict_all_emoji'])
+            results_global.counter_total_tweets += results.counter_total_tweets
+            results_global.counter_total_tweets_wemoji += results.counter_total_tweets_wemoji
+            results_global.counter_total_match += results.counter_total_match
+            results_global.counter_total_before += results.counter_total_before
+            results_global.counter_total_after += results.counter_total_after
+            results_global.counterdict_before = sum_dicts(
+                results_global.counterdict_before, results.counterdict_before
+            )
+            results_global.counterdict_after = sum_dicts(
+                results_global.counterdict_after, results.counterdict_after
+            )
+            results_global.counterdict_lang = sum_dicts(
+                results_global.counterdict_lang, results.counterdict_lang
+            )
+            results_global.counterdict_all_emoji = sum_dicts(
+                results_global.counterdict_all_emoji, results.counterdict_all_emoji
+            )
 
     except KeyboardInterrupt:
         print("KeyboardInterrupt")
     finally:
         pool.terminate()
         pool.join()
-
     end_t = timer()
-
-    # Print outputs
+    # Print outputs of the search run
     print("Elapsed Time          : {:.2f} min".format((end_t - start_t) / 60))
-    print("Total Tweets          : {:d}".format(counter_total_tweets))
-    print("Total Tweets w/ Emoji : {:d}".format(counter_total_tweets_wemoji))
-    print("Total Matches         : {:d}".format(counter_total_match))
-    print("Total w/ Before       : {:d}".format(counter_total_before))
-    print("Total w/ After        : {:d}".format(counter_total_after))
+    print("Total Tweets          : {:d}".format(results_global.counter_total_tweets))
+    print("Total Tweets w/ Emoji : {:d}".format(results_global.counter_total_tweets_wemoji))
+    print("Total Matches         : {:d}".format(results_global.counter_total_match))
+    print("Total w/ Before       : {:d}".format(results_global.counter_total_before))
+    print("Total w/ After        : {:d}".format(results_global.counter_total_after))
 
+    return results_global
+
+
+def save_results(results):
+    """Save results to csv."""
     # Convert output to dataframe
     df_before = pd.DataFrame(
-        list(counterdict_before.items()), columns=['Emoji', 'CountBefore'])
+        list(results.counterdict_before.items()), columns=["Emoji", "CountBefore"]
+    )
     df_after = pd.DataFrame(
-        list(counterdict_after.items()), columns=['Emoji', 'CountAfter'])
-    df_lang = pd.DataFrame(
-        list(counterdict_lang.items()), columns=['Lang', 'Count'])
+        list(results.counterdict_after.items()), columns=["Emoji", "CountAfter"]
+    )
+    df_lang = pd.DataFrame(list(results.counterdict_lang.items()), columns=["Lang", "Count"])
     df_allemoji = pd.DataFrame(
-        list(counterdict_all_emoji.items()), columns=['Emoji', 'Count'])
+        list(results.counterdict_all_emoji.items()), columns=["Emoji", "Count"]
+    )
 
     # Merge before and after dataframes
-    df_all = pd.merge(df_before, df_after, on='Emoji', how='outer')
-
-    df_all.sort_values('CountBefore', ascending=False).head()
-    df_all.sort_values('CountAfter', ascending=False).head()
-    df_lang.sort_values('Count', ascending=False).head()
-    df_all.sort_values('CountBefore',
-                       ascending=False).head(20).plot.bar(y='CountBefore')
-    df_all.sort_values('CountAfter',
-                       ascending=False).head(20).plot.bar(y='CountAfter')
+    df_all = pd.merge(df_before, df_after, on="Emoji", how="outer")
 
     # Export results as CSV files
-    df_all.to_csv("./alldata.csv", encoding='utf-8')
-    df_lang.to_csv("./langdata.csv", encoding='utf-8')
-    df_allemoji.to_csv("./allemojidata.csv", encoding='utf-8')
+    df_all.to_csv("./alldata.csv", encoding="utf-8")
+    df_lang.to_csv("./langdata.csv", encoding="utf-8")
+    df_allemoji.to_csv("./allemojidata.csv", encoding="utf-8")
+
+
+if __name__ == "__main__":
+
+    args = parse_cli_args()
+
+    # Character to match
+    MATCH = EMOJI_UNICODE[":pistol:"]
+
+    # Unpack and list all files
+    unpack_files(args.data_path)
+    all_files = get_all_files(args.data_path, days=args.days, hours=args.hours)
+
+    # # Main search loop
+    # RESULTS_GLOBAL = run()
+    # save_results(RESULTS_GLOBAL)
